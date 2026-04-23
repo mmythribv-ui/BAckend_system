d1a3723d71f2f597d7281aba73de4ea057cddd16
mythrivb-ui <mmythribv@gmail.com>
2026-04-23 13:15:16 +0530

    Fixed Render port issue

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ connectToMongo();
 
 const express = require('express')
 const app = express()
-const port = 3001
 
 const cors = require('cors')
 const router = require('./Routes/router')
@@ -12,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
+const port = process.env.PORT || 3001;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
